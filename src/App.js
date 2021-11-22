bc458a1d51d669b6adc316b5b95aea7887336131
farhatamiine <farhatamiine@gmail.com>
2021-11-22 12:33:05 +0100

    Resume first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Box, ChakraProvider, Grid, theme } from '@chakra-ui/react';
+import React from 'react';
+import { ColorModeSwitcher } from './components/ColorModeSwitcher';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
